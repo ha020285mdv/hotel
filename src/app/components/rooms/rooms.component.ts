32ae15aa9c048e244babb7798e43612fffe58644
Dmytro Mykytiuk <dmytro.mykytiuk@tietoevry.com>
2023-06-06 15:31:14 +0200

    components

---
+++ src/app/components/rooms/rooms.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.css']
+})
+export class RoomsComponent implements OnInit {
+  
+  hideRooms = false;
+
+  hotelName = 'Scandic';
+
+  numberOfRoms = 99;
+
+  constructor() {}
+  
+  ngOnInit(): void {
+  }
+
+  toggle() {
+    this.hideRooms = !this.hideRooms;
+  }
+
+}
